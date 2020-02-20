92976956d702d778eeec3009143f3aae691b1e25
Mathias <mhham592@gmail.com>
2020-02-19 18:52:48 -0600

    crop top

--- docScanner.py
+++ docScanner.py
@@ -3,52 +3,77 @@ import numpy as np
 import argparse
 import cv2
 import imutils
+import math
 
 class Group:
-	def __init__(self,e,i):
-		self.edges=e[:]
-		self.im = i
-		self.obb = None
-		self.aabb = np.zeros((2, 2), np.int32)
-		self.aabb[0][0]=self.im.shape[0]
-		self.aabb[0][1]=self.im.shape[1]
+        def __init__(self,e,i):
+                self.edges=e[:]
+                self.im = i
+                self.obb = None
+                self.aabb = np.zeros((2, 2), np.int32)
+                self.aabb[0][0]=self.im.shape[0]
+                self.aabb[0][1]=self.im.shape[1]
 
-		#first, find the aabb for the group of edges
-		for e in self.edges:
-			#x min & max
-			if e[0][0]<self.aabb[0][0]:
-				self.aabb[0][0]=e[0][0]
-			elif e[0][0]>self.aabb[1][0]:
-				self.aabb[1][0]=e[0][0]
-			#y min & max
-			if e[0][1]<self.aabb[0][1]:
-				self.aabb[0][1]=e[0][1]
-			elif e[0][1]>self.aabb[1][1]:
-				self.aabb[1][1]=e[0][1]
+                #first, find the aabb for the group of edges
+                for e in self.edges:
+                        #x min & max
+                        if e[0][0]<self.aabb[0][0]:
+                                self.aabb[0][0]=e[0][0]
+                        elif e[0][0]>self.aabb[1][0]:
+                                self.aabb[1][0]=e[0][0]
+                        #y min & max
+                        if e[0][1]<self.aabb[0][1]:
+                                self.aabb[0][1]=e[0][1]
+                        elif e[0][1]>self.aabb[1][1]:
+                                self.aabb[1][1]=e[0][1]
 
-		self.obb = cv2.minAreaRect(self.edges)
+                self.obb = cv2.minAreaRect(self.edges)
 
-	def display(self):
-		box = cv2.boxPoints(self.obb) # cv2.boxPoints(rect) for OpenCV 3.x
-		box = np.int0(box)
-		bb = np.zeros((4,2), np.int32)
-		bb[0] = self.aabb[0]
-		bb[1] = (self.aabb[0][0], self.aabb[1][1])
-		bb[2] = self.aabb[1]
-		bb[3] = (self.aabb[1][0], self.aabb[0][1])
-		cv2.drawContours(self.im, self.edges, -1, (0,255,255), 1)
-		cv2.polylines(self.im, [bb], True, (255,0,0), 1)
-		cv2.drawContours(self.im, [box], 0, (0,0,255), 1)
-		cv2.imshow("Group", self.im)
+        def display(self):
+                box = cv2.boxPoints(self.obb) # cv2.boxPoints(rect) for OpenCV 3.x
+                box = np.int0(box)
+                bb = np.zeros((4,2), np.int32)
+                bb[0] = self.aabb[0]
+                bb[1] = (self.aabb[0][0], self.aabb[1][1])
+                bb[2] = self.aabb[1]
+                bb[3] = (self.aabb[1][0], self.aabb[0][1])
+                cv2.drawContours(self.im, self.edges, -1, (0,255,255), 1)
+                cv2.polylines(self.im, [bb], True, (255,0,0), 1)
+                cv2.drawContours(self.im, [box], 0, (0,0,255), 1)
+                
+                print("BOX: ", box)
+                min_x = math.inf
+                min_y = math.inf
+                max_x = -1
+                max_y = -1
+                for points in box:
+                        x = points[0]
+                        y = points[1]
+                        if x < min_x:
+                                min_x = x
+                        if y < min_y:
+                                min_y = y
+                        if x > max_x:
+                                max_x = x
+                        if y > max_y:
+                                max_y = y
+                print("Min x: ", min_x, " Max x: " , max_x)
+                print("Min y: ", min_y, " Max y: " , max_y)
 
-	def envelope(self, b):
-		#FIXME: use OBB
-		return b.aabb[0][0]>=self.aabb[0][0] and b.aabb[0][1]>=self.aabb[0][1] and b.aabb[1][0]<=self.aabb[1][0] and b.aabb[1][1]<=self.aabb[1][1]
+                crop_img = self.im[min_y:max_y, min_x:max_x].copy()
 
-	def overlap(self, b):
-		#overlapping groups should be merged
-		return False
-		
+                cv2.imshow("Crop" , crop_img)
+
+                cv2.imshow("Group", self.im)
+
+        def envelope(self, b):
+                #FIXME: use OBB
+                return b.aabb[0][0]>=self.aabb[0][0] and b.aabb[0][1]>=self.aabb[0][1] and b.aabb[1][0]<=self.aabb[1][0] and b.aabb[1][1]<=self.aabb[1][1]
+
+        def overlap(self, b):
+                #overlapping groups should be merged
+                return False
+                
 
 #the group pools are used by the worker threads (lists in CPython are 
 #inherently thread-safe because of the GIL)
@@ -75,9 +100,9 @@ edged = cv2.Canny(blurred, 75, 200)
 
 
 def create_groups(edges, grayImage):
-	for e in edges:
-		g = Group(e, grayImage)
-		group_pool_A.append(g)
+        for e in edges:
+                g = Group(e, grayImage)
+                group_pool_A.append(g)
 
 
 print("STEP 1: EDGE DETECTION")
@@ -90,12 +115,12 @@ contours, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_AP
 cont = contours;
 #print(type(contours[0]))
 #for c in contours:
-#	peri = cv2.arcLength(c, True)
-#	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+#        peri = cv2.arcLength(c, True)
+#        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 #
-#	if len(approx) == 4:
-#		cont = [approx]
-#		break
+#        if len(approx) == 4:
+#                cont = [approx]
+#                break
 
 
 print("Number of contours found =" + str(len(contours)))
@@ -110,13 +135,13 @@ group_pool_A.reverse()
 #simplify, remove groups that are entirely within other groups
 trash = []
 for i,g in enumerate(group_pool_A):
-	for h in group_pool_A[i+1:]:
-		if h not in trash and g.envelope(h):
-			trash.append(h)
+        for h in group_pool_A[i+1:]:
+                if h not in trash and g.envelope(h):
+                        trash.append(h)
 
 for g in group_pool_A:
-	if g not in trash:
-		group_pool_B.append(g)
+        if g not in trash:
+                group_pool_B.append(g)
 
 #TODO: simplify each group further by making the edges "square", using the OBB 
 #to remove points that aren't near the straight edge
@@ -127,7 +152,8 @@ group_pool_A = [group_pool_B.pop(0)]
 #at this point we have our two groups, we can unleash the worker threads on 
 #them and find the matching edges
 for i,g in enumerate(group_pool_B):
-	g.display()
+        g.display()
+        cv2.waitKey(0)
 group_pool_A[0].display()
 
 
